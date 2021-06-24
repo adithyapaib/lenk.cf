40af06b7b543977a9f53e6034a0c61f2d720ef0a
Adithya Pai B <paiadithya26@gmail.com>
2021-06-24 22:39:53 +0530

    Optimized code 🐱‍🏍

--- api/p.js
+++ api/p.js
@@ -1,40 +1,11 @@
-import  mongoose  from "mongoose"
-require('dotenv').config()
+import  mongoose  from "mongoose";
+require('dotenv').config();
 import ShortUrl from "../models/model";
-function makeid(length) {
-    var result           = [];
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-      result.push(characters.charAt(Math.floor(Math.random() * 
-  charactersLength)));
-   }
-   return result.join('');
-  }
- 
+function makeid(r){for(var a=[],n="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",o=n.length,t=0;t<r;t++)a.push(n.charAt(Math.floor(Math.random()*o)));return a.join("")}
 export default async(req, res)=> {
-    await mongoose.connect(process.env.DB, {
-        dbName: 'lenk-cf',
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-    })
-   let encoded = await req.url.substr(3);
-   let decoded = decodeURI(encoded);
-   let URL = decoded;
-   console.log("P.JS URL is"+ URL)
-   var FinalURL ;
-   var ht;
-   if ( URL.indexOf("https") == 0 )
-   ht= "https://"
-   else
-   ht="http://"
-   URL = URL.split(":/").pop();
-   if(URL[0]=='/')
-    URL =URL.substr(1);
-    FinalURL = ht+URL
-    console.log("Final url is " + FinalURL);
-   let urlExists = await ShortUrl.findOne({url : FinalURL});
+    await mongoose.connect(process.env.DB, {dbName: 'lenk-cf',useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex: true,})
+   var finalURL = decodeURIComponent( await req.url.substr(3));
+   let urlExists = await ShortUrl.findOne({url : finalURL});
    if(urlExists)
   await res.json(urlExists.shortId)
   else
@@ -43,10 +14,8 @@ export default async(req, res)=> {
     let idExists = await ShortUrl.findOne({shortId : shortId});
     if(idExists)
     shortId = makeid(4);
-    let shortUrl = await new ShortUrl({ url: FinalURL, shortId: shortId })
+    let shortUrl = await new ShortUrl({ url: finalURL, shortId: shortId })
     let result = await shortUrl.save()
    await res.json(result.shortId);
   }
-  
-   
   }
