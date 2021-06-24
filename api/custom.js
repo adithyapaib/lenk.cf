dafed0128323aaa022d81f2c2581df5e4c33a953
Adithya Pai B <paiadithya26@gmail.com>
2021-06-24 22:38:37 +0530

    Fixed Bugs 🪲

--- api/custom.js
+++ api/custom.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 require("dotenv").config();
 import ShortUrl from "../models/model";
+function digits_count(n) {var count = 0;if (n >= 1) ++count;while (n / 10 >= 1) {n /= 10;++count;}return count;}
 export default async (req, res) => {
   await mongoose.connect(process.env.DB, {
     dbName: "lenk-cf",
@@ -8,30 +9,19 @@ export default async (req, res) => {
     useUnifiedTopology: true,
     useCreateIndex: true,
   });
-  let encoded = await req.url.substr(3);
-  let URL = decodeURI(encoded);
-  var FinalURL;
-  var ht;
-  if (URL.indexOf("https") == 0) ht = "https://";
-  else ht = "http://";
-  URL = URL.split(":/").pop();
-  if (URL[0] == "/") URL = URL.substr(1);
-  FinalURL = ht + URL;
-  let splitedID;
-  splitedID = URL.split("%3A%3A%3A69").pop();
-  console.log(splitedID);
-  splitedID = splitedID.split("&").shift();
-  console.log(splitedID);
-  // A slug %3A%3A%3A69 is added in ordered to split the url from the custom short ID
-  FinalURL = FinalURL.split("%3A%3A%3A69").shift();
-  let urlExists = await ShortUrl.findOne({ shortId: splitedID }).exec(); //Check if custom shortID already exists in Database
+  let longURL = decodeURIComponent(await req.url.substr(8));
+  let customIdLength = parseInt(longURL.split("_", 3)[0]);
+  console.log(customIdLength);
+  let digiLen = digits_count(customIdLength);
+  let customId = longURL.substring(digiLen + 1, digiLen + 1 + customIdLength);
+  let URL = longURL.substring(digiLen + 1 + customIdLength);
+  console.log(URL);
+  let urlExists = await ShortUrl.findOne({ shortId: customId }).exec();
   if (await urlExists) {
-    //If the custom ID is NOT unique which means there already EXISTS a shortId with the sent SHORTID
-    await res.json(false); //The shortID and url has confilicts and cant be stored to Database
+    await res.json(0);
   } else {
-    //If the custom ID is unique then the shortID is registered with urm and true is returned
-    let shortUrl = await new ShortUrl({ url: FinalURL, shortId: splitedID });
+    let shortUrl = await new ShortUrl({ url: URL, shortId: customId });
     let result = await shortUrl.save();
-    await res.json(true); //The shortID and url is successfully stored into database
+    await res.json(customId);
   }
 };
