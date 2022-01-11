f4bc73490a532fc16c9fd6d56f62696ad0eb4389
Adithya Pai B <paiadithya26@gmail.com>
2022-01-11 19:40:56 +0530

    removed redis

--- api/all.js
+++ api/all.js
@@ -1,28 +1,13 @@
 import  mongoose  from "mongoose";
 import ShortUrl from "../models/model";
-import redis from "redis";
 import util from 'util';
 require('util.promisify').shim();
 require('dotenv').config()
-const client = redis.createClient({
-    host: process.env.REDISHOST,
-    port: "12891",
-    password: process.env.REDISPASSWORD,
-  });
-  client.on("error", (err) => {
-    console.log("Error " + err);
-  });
+
 export default async (req, res ) =>
 {
     
-  client.get = util.promisify(client.get);
-  client.set = util.promisify(client.set);
-  const isPresentInRedis = await client.get("redisdata");
-  if (isPresentInRedis) {
-    res.json(JSON.parse(isPresentInRedis));
-    console.log("Redis Data");
-    return;
-  }
+  
   console.log("No data in redis");
     await mongoose.connect(process.env.DB, {
         dbName: 'lenk-cf',
