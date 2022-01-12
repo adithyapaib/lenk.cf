b7391bf2c796748528fc20d65a38097f1ea882a6
Adithya Pai B <paiadithya26@gmail.com>
2022-01-12 19:30:27 +0530

    Added http to urls without http

--- api/redirect.js
+++ api/redirect.js
@@ -16,6 +16,11 @@ export default async (req, res) => {
     await res.redirect("/404");
   } else {
     let url = await result.url;
+    /* check if url has https else append it  */
+    if (url.substr(0, 4) != "http"  ) {
+      url = "https://" + url;
+    }
+    
     await res.redirect(url);
   }
 };
